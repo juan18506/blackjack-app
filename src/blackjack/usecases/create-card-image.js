b08d1cb1c5622cc7a4330056ef7b28880bf349eb
Juan Alves Pedreira <juan185067@gmail.com>
2023-10-12 04:45:34 -0300

    solved a bug on card image src

--- src/blackjack/usecases/create-card-image.js
+++ src/blackjack/usecases/create-card-image.js
@@ -7,7 +7,7 @@ export const createCardImage = (card) => {
   if (!card) throw new Error('card is required');
 
   const imgCard = document.createElement('img');
-  imgCard.src = `/cards/${card}.png`;
+  imgCard.src = `./cards/${card}.png`;
   imgCard.classList.add('blackjack-card');
 
   return imgCard;
